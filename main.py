91f1d4ec9b6401957605723f224ac74095309005
Anan Li <li0047@hz.nl>
2025-03-22 15:16:30 +0100

    init

---
+++ main.py
@@ -0,0 +1,46 @@
+from tkinter import *
+import settings
+import utils
+from cell import Cell
+
+root = Tk()
+root.configure(bg="white")
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.title("Minesweeper")
+root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg="white",
+    width=settings.WIDTH, 
+    height=utils.height_prct(25)
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg="white",
+    width=utils.width_prct(25), 
+    height=utils.height_prct(75)
+)
+left_frame.place(x=0, y=utils.height_prct(25))
+
+center_frame = Frame(
+    root,
+    bg="white",
+    width=utils.width_prct(75), 
+    height=utils.height_prct(75)
+)
+center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
+
+# Build the game board
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell(x, y)
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(column=x, row=y, padx=0, pady=0)
+
+Cell.randomize_mines()
+
+
+root.mainloop()
